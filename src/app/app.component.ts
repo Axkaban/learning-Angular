47c6277a9b284178fa31500bf04128d157a52e7c
axel kaban <y>
2017-07-30 22:14:58 -0400

    learning routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    
+    
 })
-export class AppComponent {
-  title = 'Tour of Heroes';
 
-  heroes = HEROES;
-  
-  hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
-  selectedHero: Hero;
-
-  onSelect(hero: Hero): void {
-  this.selectedHero = hero;
+export class AppComponent{
+    title = 'Tour of Heroes';
 }
-}
-
-export class Hero {
-  id: number;
-  name: string;
-}
-
-
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
